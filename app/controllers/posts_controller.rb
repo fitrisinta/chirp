3d9b4c74658c82bc77d3409fc0a00a09350dcad1
Fitri Sinta <50938305+fitrisinta@users.noreply.github.com>
2020-09-29 21:15:52 +0700

    Added the features to edit and delete a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  
+
   def index
     # Use the order method to sort the posts by time created
     @posts = Post.all.order(created_at: :desc) 
@@ -21,4 +21,22 @@ class PostsController < ApplicationController
     redirect_to("/posts/index")
   end
 
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    # Update the post with the data sent from the form
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def destroy
+     # Get the post with params[:id] as the id, then destroy it
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("posts/index")
+  end
 end
