0c82735df6b46b7892d7f4ec4b15473f8c08c4c5
Fitri Sinta <50938305+fitrisinta@users.noreply.github.com>
2020-10-03 23:09:24 +0700

    Added profile image feature for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name: params[:name], email: params[:email])
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      image_name: "default_user.jpg"
+    )
     if @user.save
       flash[:notice] = "You have signed up successfully"
       redirect_to("/users/#{@user.id}")
@@ -31,6 +35,12 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}", image.read)
+    end
+
     if @user.save
       flash[:notice] = "Your account has been updated successfully"
       redirect_to("/users/#{@user.id}")
